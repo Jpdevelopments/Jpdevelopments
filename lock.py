569c00d76a027ea9fce62512cf1575ec02e6a98e
Jpdevelopments <116903950+Jpdevelopments@users.noreply.github.com>
2023-01-26 09:13:29 +0530

    Add files via upload

---
+++ lock.py
@@ -0,0 +1,8 @@
+while True:
+    user=input("Enter the password : ")
+    if user=="password":
+        print("correct password")
+        break
+    else:
+        print("wrong password")
+        
